07a871ec822afc2fe7d64755b708ff82616a00d8
Lwolwo <zuihouyiye0704@126.com>
2020-07-23 19:07:47 +0800

    [luopeijun] feat: finish exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,23 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  }
+
+  const temp = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        temp.push(array[i][j]);
+      }
+    } else {
+      temp.push(array[i]);
+    }
+  }
+  return temp;
+
+  // return Array.prototype.flat.call(array, 1);
 
   throw new Error('Please delete this line and implement the function');
 }
