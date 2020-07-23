b1a407ba27b7adadea6cbab4f381b31ccaa68de2
Lwolwo <zuihouyiye0704@126.com>
2020-07-23 18:23:38 +0800

    [luopeijun] feat: finish exercise_03

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,6 +6,24 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
+  const array = [];
+  if (start === end) {
+    return array;
+  }
+
+  if (end > start) {
+    for (let i = start; i < end; i += 1) {
+      array.push(i);
+    }
+    return array;
+  }
+
+  if (end < start) {
+    for (let i = start; i > end; i -= 1) {
+      array.push(i);
+    }
+    return array;
+  }
 
   throw new Error('Please delete this line and implement the function');
 }
@@ -17,6 +35,15 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (numbers.length === 0) {
+    return 0;
+  }
+
+  let count = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    count += numbers[i];
+  }
+  return count;
 
   throw new Error('Please delete this line and implement the function');
 }
