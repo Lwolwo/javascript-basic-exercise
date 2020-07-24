ef4aaa4cff04ef420cd7e1f24b3f55372d3a218c
Lwolwo <zuihouyiye0704@126.com>
2020-07-24 18:08:06 +0800

    [luopeijun] feat: finish exercise_07

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -9,5 +9,34 @@ export default function waitForAll(...promises) {
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
 
+  for (let i = 0; i < promises.length; i += 1) {
+    if (!(promises[i] instanceof Promise)) {
+      throw new Error('Not all elements are promises.');
+    }
+  }
+  const results = [];
+  const errors = [];
+
+  // 使用一个数组记录是否出现reject
+  for (let i = 0; i < promises.length; i += 1) {
+    errors[i] = false;
+    results.push(
+      new Promise(resolve => resolve(promises[i]))
+        .catch((error) => {
+          errors[i] = true;
+          return error;
+        }),
+    );
+  }
+  // 等待执行完所有promise，再去判断是否有reject
+  return Promise.all(results).then((p) => {
+    const index = errors.indexOf(true);
+    if (index >= 0) {
+      throw p[index];
+    } else {
+      return p;
+    }
+  });
+
   throw new Error('Please delete this line and implement the function');
 }
